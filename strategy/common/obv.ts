d323ff9fa4546ddddfcee7a9c6bd60e870872316
Karolis Gudiškis <karolisgud@gmail.com>
2023-11-20 21:05:33 +0200

    Update obv and use crypto data structure

--- strategy/common/obv.ts
+++ strategy/common/obv.ts
@@ -10,81 +10,100 @@ let client: ActionClient = await ActionClient.new(12345);
  Exit position when one of conditions is met:
  - OBV < 0
  - 5% profit
- - 60 days
+ - 60 price changes
 */
 
-const PROFIT_MARGIN = 0.05;
-const MAX_DAYS = 60;
+const PROFIT_MARGIN = 0.01;
+const MAX_PRICE_CHANGES = 3;
 
-let previousDayPrice = null;
-let obv = 0;
+let previousDayPrice: {[key: string]: number} = {};
+let obv: {[key: string]: number} = {};
 
-let entryPoint = {
-    price: 0,
-    date: null,
+let entryPoint: {[key: string]: {price: number, date: Date, priceChanges: number}} = {
 };
 
-let testEntryCondition = () => {
-    if (obv > 0) {
+let testEntryCondition = (ticker: string) => {
+    if (obv[ticker] > 0) {
         return true;
     }
 };
 
-let testExitCondition = (obj) => {
-    if (obv < 0) {
+let testExitCondition = (ticker: string, price: number) => {
+    if (obv[ticker] < 0) {
+        console.log(`${ticker} exiting because of obv < 0`);
         return true;
     }
 
-    if (entryPoint.price && obj.new.Close > entryPoint.price * (1 + PROFIT_MARGIN)) {
+    if (entryPoint[ticker]?.price && price > entryPoint[ticker]?.price * (1 + PROFIT_MARGIN)) {
+        console.log(`${ticker} exiting because of profit ${PROFIT_MARGIN * 100}% reached`);
         return true;
     }
 
 
-    if (entryPoint.date) {
-        const diffTime = Math.abs(new Date(obj.new.Date) - new Date(entryPoint.date));
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if (entryPoint[ticker]?.priceChanges > MAX_PRICE_CHANGES) {
+        console.log(`${ticker} exiting because of max price changes reached`);
+        return true;
+    }
+};
 
-        if (diffDays > MAX_DAYS) {
-            return true;
+let balance: {[key: string]: number} = {};
+
+interface Value {
+    new: {
+        value: {
+            ticker: string,
+            price_in_dollar: string,
+            volume: string,
+            time: number,
+            asset_type: string
         }
     }
-};
+}
+const run = async (obj: Value) => {
+    let newValue = obj.new.value;
+    let price = parseFloat(newValue.price_in_dollar);
+    let volume = parseFloat(newValue.volume);
+    let date = new Date(newValue.time);
+    let ticker = newValue.ticker;
 
-let balance = 1000;
-
-const run = async (obj: any) => {
-    if (previousDayPrice !== null) {
-        if (obj.new.Close > previousDayPrice) {
-            obv += obj.new.Volume;
-        } else if (obj.new.Close === previousDayPrice) {
-            obv = obj.new.Volume;
+    if (previousDayPrice[ticker]) {
+        if (price > previousDayPrice[ticker]) {
+            obv[ticker] = (obv[ticker] ?? 0) + volume;
+        } else if (price === previousDayPrice[ticker]) {
+            obv[ticker] = volume;
         } else {
-            obv -= obj.new.Volume;
-        }
-    }
-
-    previousDayPrice = obj.new.Close;
-
-    console.log(`${obj.new.Date} OBV ${obv}`);
-
-    if (entryPoint.date === null) {
-        if (testEntryCondition()) {
-            entryPoint.price = obj.new.Close;
-            entryPoint.date = obj.new.Date;
-            console.log(`Entry point ${entryPoint.price} ${entryPoint.date}`);
-            await client.buy(new Date(obj.new.Date), "AAPL", obj.new.Close);
+            obv[ticker] = (obv[ticker] ?? 0) - volume;
         }
     } else {
-        if (testExitCondition(obj)) {
-            balance *= obj.new.Close / entryPoint.price;
+        balance[ticker] = 1000;
+    }
 
-            console.log(`Exit point ${obj.new.Close} ${obj.new.Date}`);
-            console.log(`Balance ${balance}`);
+    previousDayPrice[ticker] = price;
 
-            entryPoint.price = 0;
-            entryPoint.date = null;
+    if (entryPoint[ticker]?.date) {
+        if (testExitCondition(ticker, price)) {
+            balance[ticker] *= price / entryPoint[ticker].price;
 
-            await client.sell(new Date(obj.new.Date), "AAPL", obj.new.Close);
+            console.log(`Exit point ${ticker} ${price} ${volume}`);
+            console.log(`Balance ${ticker} ${balance[ticker]}`);
+
+            entryPoint[ticker].price = 0;
+            entryPoint[ticker].date = null;
+
+            await client.sell(date, ticker, price);
+        } else {
+            entryPoint[ticker].priceChanges += 1;
+        }
+    } else {
+        if (testEntryCondition(ticker)) {
+            entryPoint[ticker] = {
+                price,
+                date,
+                priceChanges: 0
+            };
+
+            console.log(`Entry point ${ticker} ${entryPoint[ticker].price} ${entryPoint[ticker].date}`);
+            await client.buy(date, ticker, price);
         }
     }
 }

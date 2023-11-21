0c5cfcb6e0f60434bb25826781b942a2656eda06
chubei <914745487@qq.com>
2023-11-22 00:36:23 +0800

    Add ma12 indicator and ticker filter

---
+++ strategy/indicators/ma12.ts
@@ -0,0 +1,27 @@
+import { Value } from "../common/interface";
+
+class Ma {
+    history: number[] = [];
+
+    constructor(readonly windowSize: number) {}
+
+    process(value: number): number | null {
+        this.history.push(value);
+
+        if (this.history.length > this.windowSize) {
+            this.history.shift();
+        }
+
+        if (this.history.length === this.windowSize) {
+            return this.history.reduce((a, b) => a + b, 0) / this.windowSize;
+        }
+
+        return null;
+    }
+}
+
+const ma12 = new Ma(12);
+
+export default function(value: Value) {
+    return ma12.process(value.price_in_dollar);
+}

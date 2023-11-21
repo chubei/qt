8879ebc7e37a33fcc7cb9aaed3ed3f67ddc642c0
chubei <914745487@qq.com>
2023-11-22 02:17:38 +0800

    sort price by time. calculate net worth in sql

--- strategy/ingest_price.js
+++ strategy/ingest_price.js
@@ -31,6 +31,14 @@ async function fetchDataAndIngest() {
   } else if (mode === "history") {
     const tickers = ["AAPL", "MSFT", "AMZN", "GOOG", "BABA"];
 
+    const snapshot_msg = {
+      typ: "SnapshottingDone",
+      old_val: null,
+      new_val: null,
+    };
+    await Deno[Deno.internal].core.ops.ingest(snapshot_msg);
+
+    const allRecords = [];
     for (const ticker of tickers) {
       const url =
         "http://api.marketstack.com/v1/eod?access_key=3b0b27edd5fcea60cf6b6245af317e87&sort=ASC&limit=1000&symbols=" +
@@ -39,28 +47,29 @@ async function fetchDataAndIngest() {
       const response = await fetch(url);
       const result = await response.json();
       const data = result.data;
-
-      const snapshot_msg = {
-        typ: "SnapshottingDone",
-        old_val: null,
-        new_val: null,
-      };
-      await Deno[Deno.internal].core.ops.ingest(snapshot_msg);
-      console.log("Ingested");
       for (const record of data) {
-        const msg = {
-          typ: "Insert",
-          old_val: null,
-          new_val: {
-            ticker: record.symbol,
-            price_in_dollar: record.close,
-            volume: record.volume,
-            time: record.date,
-            asset_type: "Stocks",
-          },
-        };
-        await Deno[Deno.internal].core.ops.ingest(msg);
+        allRecords.push(record);
       }
+    }
+
+    // Sort the records by date
+    allRecords.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+
+    for (const record of allRecords) {
+      const msg = {
+        typ: "Insert",
+        old_val: null,
+        new_val: {
+          ticker: record.symbol,
+          price_in_dollar: record.open,
+          volume: record.volume,
+          time: record.date,
+          asset_type: "Stocks",
+        },
+      };
+      await Deno[Deno.internal].core.ops.ingest(msg);
     }
   }
 }
